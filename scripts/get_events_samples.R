3b9a858334b4efd64c13e65438820a77c72cba41
Augustina Ragwitz <augustina.ragwitz@ibm.com>
2017-02-27 21:54:35 -0800

    Final report for events to actors analysis of Github events
    * final draft for analysis of correlation between actor and event frequency
    * split the old events analysis into seperate parts
    
    Closes Issue #48
    Signed-off-by: Augustina Ragwitz <augustina.ragwitz@ibm.com>

--- scripts/get_events_samples.R
+++ scripts/get_events_samples.R
@@ -4,49 +4,37 @@ library(dplyr)
 library(bigrquery)
 
 project <- "bonnyci-github-archive"
-num_samples <- 1
+num_samples <- 5
 sample_size <- 100
+participation_rate <- "low"
 
-# Get the high end of the range
-high_count_sql <-
-  'select count(*) as count from [bonnyci-github-archive:ci_plunder_events.high_participation_repo_id]'
-high_count <- query_exec(high_count_sql, project = project)
+repo_count_sql <-
+  paste('select count(*) as count from [bonnyci-github-archive:ci_plunder_events.', participation_rate, '_participation_repo_id]', sep="")
+repo_count <- query_exec(repo_count_sql, project = project)
 
-# Get the high end of the range
-med_count_sql <-
-  'select count(*) as count from [bonnyci-github-archive:ci_plunder_events.high_participation_repo_id]'
-med_count <- query_exec(med_count_sql, project = project)
 
-for (sample_number in range(1,num_samples)){
+
+for (sample_number in 1:num_samples){
   # Generate the random numbers and build the data sets
-  high_sample_ids <- sort(sample(high_count$count, sample_size, replace=FALSE))
-  med_sample_ids <- sort(sample(high_count$count, sample_size, replace=FALSE))
+  sample_ids <- sort(sample(repo_count$count, sample_size, replace=FALSE))
   
   # Get the list of project names so we can get the dataz
-  high_sample_repos_sql <-
-    paste('SELECT repo_name FROM [bonnyci-github-archive:ci_plunder_events.high_participation_repo_id] WHERE repo_id IN(',
-          paste(as.character(high_sample_ids), collapse=", "),
-          ')',
-          sep=" ")
-  med_sample_repos_sql <-
-    paste('SELECT repo_name FROM [bonnyci-github-archive:ci_plunder_events.med_participation_repo_id] WHERE repo_id IN(',
-          paste(as.character(med_sample_ids), collapse=", "),
+  sample_repos_sql <-
+    paste(paste('SELECT repo_name FROM [bonnyci-github-archive:ci_plunder_events.', participation_rate, '_participation_repo_id] WHERE repo_id IN(', sep=""),
+          paste(as.character(sample_ids), collapse=", "),
           ')',
           sep=" ")
   
-  high_sample_repos <- query_exec(high_sample_repos_sql, project = project)
-  med_sample_repos <- query_exec(med_sample_repos_sql, project = project)
-  
-  sample_repos <- bind_rows(high_sample_repos, med_sample_repos)
+  sample_repos <- query_exec(sample_repos_sql, project = project)
   
   # Get the actual data for each repo
   sample_sql <-
-    paste("SELECT * FROM [bonnyci-github-archive:ci_plunder_events.participation_rate_per_repo] WHERE repo_name IN('",
+    paste("SELECT * FROM [bonnyci-github-archive:ci_plunder_events.repos_participation_rate_event_types_summary] WHERE repo_name IN('",
           paste(as.character(sample_repos$repo_name), collapse="','"),
           "')", sep="")
   sample <- query_exec(sample_sql, project = project)
   
-  sample_file <- paste("repos_sample_", sample_number, sep="")
+  sample_file <- paste(participation_rate, "_repos_sample_", sample_number, sep="")
   saveRDS(sample, paste(sample_file, ".rds", sep=""))
   write.csv(sample, paste(sample_file, ".csv", sep=""))
 }
